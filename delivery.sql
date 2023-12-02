5887dc2b1a9068c61e2c1bd46f7542ab2e2d37e5
MENDOZA <44247358+mendozabernillanilsen10@users.noreply.github.com>
2023-12-01 20:12:33 -0500

    sss
    sss

--- delivery.sql
+++ delivery.sql
@@ -1,48 +1,224 @@
--- Crear la tabla de clientes
-CREATE TABLE clientes (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(50) NOT NULL,
-    apellido VARCHAR(50) NOT NULL,
-    dni VARCHAR(20) UNIQUE NOT NULL,
-    lugar_procedencia VARCHAR(100) NOT NULL,
-    password varchar(250) DEFAULT NULL
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-12-2023 a las 02:12:21
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
 
--- Crear la tabla de hoteles
-CREATE TABLE hoteles (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(100) NOT NULL,
-    ubicacion VARCHAR(255) NOT NULL,
-    ruc VARCHAR(20) UNIQUE NOT NULL,
-    descripcion TEXT,
-    foto_url text NOT NULL
-);
--- Crear la tabla de habitaciones con campo de foto_url
-CREATE TABLE habitaciones (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    numero INT NOT NULL,
-    tipo VARCHAR(50) NOT NULL,
-    precio DECIMAL(10, 2) NOT NULL,
-    hotel_id INT,
-    foto_url VARCHAR(255), -- Añadido para la URL de la foto
-    FOREIGN KEY (hotel_id) REFERENCES hoteles(id)
-);
--- Crear la tabla de lugares turísticos con campo de foto_url
-CREATE TABLE lugares_turisticos (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(100) NOT NULL,
-    descripcion TEXT,
-    ubicacion VARCHAR(255) NOT NULL,
-    foto_url VARCHAR(255) -- Añadido para la URL de la foto
-);
--- Crear la tabla de reservas
-CREATE TABLE reservas (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    cliente_id INT,
-    habitacion_id INT,
-    fecha_inicio DATE NOT NULL,
-    fecha_fin DATE NOT NULL,
-    estado VARCHAR(20) DEFAULT 'Pendiente',
-    FOREIGN KEY (cliente_id) REFERENCES clientes(id),
-    FOREIGN KEY (habitacion_id) REFERENCES habitaciones(id)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `hotelsistemauss`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `dni` varchar(20) NOT NULL,
+  `lugar_procedencia` varchar(100) NOT NULL,
+  `password` varchar(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id`, `nombre`, `apellido`, `dni`, `lugar_procedencia`, `password`) VALUES
+(24, 'q', 'qs', 's', 's', '$2b$12$L5XFdVKvPuhdpX6/P7BKfO3xXDof/JNfOtEhVVsCjjZLML6pVRB0u'),
+(27, 'ww', 'w', 'w', 'w', '$2b$12$zZtQkGRnypvq34kJgrskZO9PzuVY1UItI7Rl771Pm6nhCjbsqcPqG');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `habitaciones`
+--
+
+CREATE TABLE `habitaciones` (
+  `id` int(11) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `tipo` varchar(50) NOT NULL,
+  `precio` text NOT NULL,
+  `hotel_id` int(11) DEFAULT NULL,
+  `foto_url` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `habitaciones`
+--
+
+INSERT INTO `habitaciones` (`id`, `numero`, `tipo`, `precio`, `hotel_id`, `foto_url`) VALUES
+(19, 2, '2', '2', 5, 'Screenshot_25.png'),
+(29, 0, 'a', 'a', 5, 'WhatsApp_Image_2023-11-30_at_10.14.17_PM.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `hoteles`
+--
+
+CREATE TABLE `hoteles` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `ubicacion` varchar(255) DEFAULT NULL,
+  `ruc` varchar(20) DEFAULT NULL,
+  `descripcion` text DEFAULT NULL,
+  `foto_url` varchar(333) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `hoteles`
+--
+
+INSERT INTO `hoteles` (`id`, `nombre`, `ubicacion`, `ruc`, `descripcion`, `foto_url`) VALUES
+(5, '1w', '1w', '1w', '1w', 'diagram-export-24-10-2023-21_59_49.png'),
+(7, 'smith', 'r', 'r', 'r', 'Screenshot_13.png'),
+(8, 'aa', 'aa', 'aa', 'aa', 'WhatsApp_Image_2023-11-30_at_10.14.17_PM.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `lugares_turisticos`
+--
+
+CREATE TABLE `lugares_turisticos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `descripcion` text DEFAULT NULL,
+  `ubicacion` varchar(255) DEFAULT NULL,
+  `foto_url` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `lugares_turisticos`
+--
+
+INSERT INTO `lugares_turisticos` (`id`, `nombre`, `descripcion`, `ubicacion`, `foto_url`) VALUES
+(11, 's', 's', 's', 'Screenshot_14.png'),
+(12, 'ss', 'ss', 'ss', 'Screenshot_16.png');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reservas`
+--
+
+CREATE TABLE `reservas` (
+  `id` int(11) NOT NULL,
+  `cliente_id` int(11) DEFAULT NULL,
+  `habitacion_id` int(11) DEFAULT NULL,
+  `fecha_inicio` date NOT NULL,
+  `fecha_fin` date NOT NULL,
+  `estado` varchar(20) DEFAULT 'Pendiente'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `dni` (`dni`);
+
+--
+-- Indices de la tabla `habitaciones`
+--
+ALTER TABLE `habitaciones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `hotel_id` (`hotel_id`);
+
+--
+-- Indices de la tabla `hoteles`
+--
+ALTER TABLE `hoteles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `ruc` (`ruc`);
+
+--
+-- Indices de la tabla `lugares_turisticos`
+--
+ALTER TABLE `lugares_turisticos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cliente_id` (`cliente_id`),
+  ADD KEY `habitacion_id` (`habitacion_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT de la tabla `habitaciones`
+--
+ALTER TABLE `habitaciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT de la tabla `hoteles`
+--
+ALTER TABLE `hoteles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `lugares_turisticos`
+--
+ALTER TABLE `lugares_turisticos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT de la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `habitaciones`
+--
+ALTER TABLE `habitaciones`
+  ADD CONSTRAINT `habitaciones_ibfk_1` FOREIGN KEY (`hotel_id`) REFERENCES `hoteles` (`id`);
+
+--
+-- Filtros para la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  ADD CONSTRAINT `reservas_ibfk_1` FOREIGN KEY (`cliente_id`) REFERENCES `clientes` (`id`),
+  ADD CONSTRAINT `reservas_ibfk_2` FOREIGN KEY (`habitacion_id`) REFERENCES `habitaciones` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
